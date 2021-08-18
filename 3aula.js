88540483ac5334d9d345aab4bdafacc055606954
Arthur de Paula <arthurhenriquep2@gmail.com>
2021-08-18 20:04:27 -0300

    18/08/2021

--- 3aula.js
+++ 3aula.js
@@ -1,40 +1,89 @@
-let gerenciadorderedes = () => {
-let vetorrd= [] 
-//Criar vetor com objetos
-for(let i=0; i<5; i++){
-
-    let redessociais = {
-        codigo: Number(prompt(`Informe o seu código:`)),
-        nome: prompt(`informe seu nome:`),
-        link: prompt(`Informe o link da rede social:`)
-
-    }
-    // Inserir objeto no vetor
-    vetorrd.push(redessociais)
-}
-let vetorus= []
-// cadastro dos usúarios
-for(let i=0; i<5; i++){
-
-    let usuarios = {
-        login: prompt(`Informe o seu login:`),
-        nomeus: prompt(`Informe seu nome por gentileza:`),
-        codigous: Number(prompt(`Informe seu código por gentileza:`)),
-        qtde: Number(prompt(`Informe a quantidade de posts por gentileza:`))
-    }
-    let achou = false // assumo que não achei
-    let j = 0 // novo indice 
-    while(!achou && j<5 ){ // sai ou pq acabou o vetor ou porque achou
-        if(vetorrd[j].codigo == usuarios.codigous){
-            vetorus.push(usuarios)
-            achou = true
-            alert(`Usuário inserido com sucesso`)
+let gerenciaRedesSociais = () => {
+    // cadastro das redes sociais
+    let vetorRedesSociais = []
+    for(let i=0;i<5;i++){
+        let objeto = {
+            codigo: Number(prompt(`Informe código da rede social`)),
+            nome: prompt(`Informe nome da rede social`),
+            url: prompt(`Informe url da rede social`)
         }
-        j++
+        // inserir objeto no vetor
+        vetorRedesSociais.push(objeto)
+    }
+    // cadastro dos usuários
+    let vetorUsuarios = []
+    let i = 0
+    while (i<5){
+        let objeto = {
+            login: prompt(`Informe o login do usuário`),
+            nome: prompt(`Informe o nome do usuário`),
+            codigoRedeSocial: Number(prompt(`Informe o código da rede social`)),
+            qtdePosts: Number(prompt(`Informe a qtde de Posts`))
+        }
+        // só faremos isso se a rede social existir
+        let achou = false // assumo que não encontrei a rede social
+        let j = 0 // índice do vetor de redes sociais
+        while (!achou && j<5 ){ // sai ou porque achou ou porque acabou o vetor
+            if (vetorRedesSociais[j].codigo == objeto.codigoRedeSocial){ // achamos
+                vetorUsuarios.push(objeto)// inserimos
+                achou = true
+                alert(`Usuário inserido com sucesso`)
+            }
+            j++ // prepara para ir ao próximo elemento
+        }
+        if (!achou){ // acabou o vetor
+            alert(`Usuário não inserido, pois a Rede social não foi encontrada`)
+        }
+        i++
     }
 
-    if (!achou){
-    alert(`Usuario não inserido, pois a rede social não foi encontrada`)
-    } 
-}
+  // cadastro dos usuários
+  /*
+  let vetorUsuarios = []
+  let i = 0
+  while (i<5){
+      let objeto = {
+          login: prompt(`Informe o login do usuário`),
+          nome: prompt(`Informe o nome do usuário`),
+          codigoRedeSocial: Number(prompt(`Informe o código da rede social`)),
+          qtdePosts: Number(prompt(`Informe a qtde de Posts`))
+      }
+      // só faremos isso se a rede social existir
+      let achou = false // assumo que não encontrei a rede social
+       // índice do vetor de redes sociais
+      for(let j = 0; j < 5; j++ ){ // sai ou porque achou ou porque acabou o vetor
+          if (vetorRedesSociais[j].codigo == objeto.codigoRedeSocial){ // achamos
+              vetorUsuarios.push(objeto)// inserimos
+              achou = true
+              alert(`Usuário inserido com sucesso`)
+              break // sai do for
+          }
+      }
+      if (!achou){ // acabou o vetor
+          alert(`Usuário não inserido, pois a Rede social não foi encontrada`)
+      }
+      i++
+  }
+  */
+  // Exe1) o usuário informa a rede social, e o programa retorna quantos posts foram feitos
+  let codigo = Number(prompt(`Informe o código da rede social`))
+  // percorre vetor procurando
+  let conta = 0
+  for(let i=0;i<5;i++){ // percore o vetor vetorUsuarios
+    if (vetorUsuarios[i].codigoRedeSocial == codigo){ // encontrei
+        conta = conta + vetorUsuarios[i].qtdePosts
+    }
+  }
+  if (conta == 0){
+      console.log(`Não houve postagem ou rede social não existe`)
+  }
+  else {
+      console.log(`A qtde de post na rede social ${codigo} foi ${conta}`)
+  }
+
+
+  // Exe2) o programa retorna quantos posts foram feitos em todas as redes sociais
+  // Exe3) o usuário informa informa o login do usuário, e o programa retorna quantos posts ele fez
+  // Exe4) o programa retorna quantos posts foram feitos por cada usuário
+
 }
